0c251480cfab91c987ca4ecf3d7b3cbe488b6c0e
JemilCodes <jemiluishaq277@gmail.com>
2023-03-21 15:28:51 +0100

    redeploying

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/votify/api/v1",
+  baseUrl: "https://votifyapi-q9zc.onrender.com/votify/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
